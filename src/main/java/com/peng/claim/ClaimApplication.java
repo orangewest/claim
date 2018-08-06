9c214810dbb8e394a336f00c4dc4b85df531e56a
orangewest <jinpengcn@foxmail.com>
2018-08-06 22:24:26 +0800

    new

---
+++ src/main/java/com/peng/claim/ClaimApplication.java
@@ -0,0 +1,12 @@
+package com.peng.claim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimApplication.class, args);
+	}
+}
